0b52ed24b36ff5c239450fd616f407d4f4ad4842
Eduardo Herrera <duds.herrera@gmail.com>
2022-09-18 03:15:38 -0300

    Adicionar arquivos de projeto.

---
+++ Projeto-Katia5/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace Projeto_Katia5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.TxtNome = new System.Windows.Forms.TextBox();
+            this.TxtPeso = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TxtIdade = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TxtAltura = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.TxtTime = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.TxtCamisa = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.BtnGravar = new System.Windows.Forms.Button();
+            this.BtnPesquisar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(147, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome :";
+            // 
+            // TxtNome
+            // 
+            this.TxtNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtNome.Location = new System.Drawing.Point(231, 65);
+            this.TxtNome.Name = "TxtNome";
+            this.TxtNome.Size = new System.Drawing.Size(313, 26);
+            this.TxtNome.TabIndex = 1;
+            // 
+            // TxtPeso
+            // 
+            this.TxtPeso.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtPeso.Location = new System.Drawing.Point(231, 111);
+            this.TxtPeso.Name = "TxtPeso";
+            this.TxtPeso.Size = new System.Drawing.Size(313, 26);
+            this.TxtPeso.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(154, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Peso :";
+            // 
+            // TxtIdade
+            // 
+            this.TxtIdade.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtIdade.Location = new System.Drawing.Point(231, 202);
+            this.TxtIdade.Name = "TxtIdade";
+            this.TxtIdade.Size = new System.Drawing.Size(313, 26);
+            this.TxtIdade.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(149, 202);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 24);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Idade :";
+            // 
+            // TxtAltura
+            // 
+            this.TxtAltura.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtAltura.Location = new System.Drawing.Point(231, 156);
+            this.TxtAltura.Name = "TxtAltura";
+            this.TxtAltura.Size = new System.Drawing.Size(313, 26);
+            this.TxtAltura.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(150, 158);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 24);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Altura :";
+            // 
+            // TxtTime
+            // 
+            this.TxtTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtTime.Location = new System.Drawing.Point(231, 294);
+            this.TxtTime.Name = "TxtTime";
+            this.TxtTime.Size = new System.Drawing.Size(313, 26);
+            this.TxtTime.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(154, 294);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(69, 24);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Time :";
+            // 
+            // TxtCamisa
+            // 
+            this.TxtCamisa.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtCamisa.Location = new System.Drawing.Point(231, 248);
+            this.TxtCamisa.Name = "TxtCamisa";
+            this.TxtCamisa.Size = new System.Drawing.Size(313, 26);
+            this.TxtCamisa.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(28, 248);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(197, 24);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Número da camisa :";
+            // 
+            // BtnGravar
+            // 
+            this.BtnGravar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnGravar.Location = new System.Drawing.Point(231, 362);
+            this.BtnGravar.Name = "BtnGravar";
+            this.BtnGravar.Size = new System.Drawing.Size(110, 39);
+            this.BtnGravar.TabIndex = 12;
+            this.BtnGravar.Text = "Gravar";
+            this.BtnGravar.UseVisualStyleBackColor = true;
+            this.BtnGravar.Click += new System.EventHandler(this.BtnGravar_Click);
+            // 
+            // BtnPesquisar
+            // 
+            this.BtnPesquisar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnPesquisar.Location = new System.Drawing.Point(434, 362);
+            this.BtnPesquisar.Name = "BtnPesquisar";
+            this.BtnPesquisar.Size = new System.Drawing.Size(110, 39);
+            this.BtnPesquisar.TabIndex = 13;
+            this.BtnPesquisar.Text = "Pesquisar";
+            this.BtnPesquisar.UseVisualStyleBackColor = true;
+            this.BtnPesquisar.Click += new System.EventHandler(this.BtnPesquisar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(681, 481);
+            this.Controls.Add(this.BtnPesquisar);
+            this.Controls.Add(this.BtnGravar);
+            this.Controls.Add(this.TxtTime);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.TxtCamisa);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.TxtIdade);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.TxtAltura);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.TxtPeso);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TxtNome);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TxtNome;
+        private System.Windows.Forms.TextBox TxtPeso;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TxtIdade;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TxtAltura;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox TxtTime;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox TxtCamisa;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button BtnGravar;
+        private System.Windows.Forms.Button BtnPesquisar;
+    }
+}
+
---
+++ Projeto-Katia5/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projeto_Katia5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Jogador jog;
+        Jogador[] v = new Jogador[3];
+        int i = 0;
+
+        private void BtnGravar_Click(object sender, EventArgs e)
+        {
+            if(i < 3)
+            {
+                jog = new Jogador();
+                jog.Nome = TxtNome.Text;
+                jog.Peso = Convert.ToDouble(TxtPeso.Text);
+                jog.Altura = Convert.ToDouble(TxtAltura.Text);
+                jog.Idade = Convert.ToInt32(TxtIdade.Text);
+                jog.Camisa = Convert.ToInt32(TxtCamisa.Text);
+                jog.Time = TxtTime.Text;
+                v[i] = jog;
+                i++;
+            }
+            else
+            {
+                MessageBox.Show("Não é possivel gravar mais dados!");
+            }
+
+        }
+
+        private void BtnPesquisar_Click(object sender, EventArgs e)
+        {
+            for(int j = 0; j < 3; j++)
+            {
+                if (v[j].Nome.Equals(TxtNome.Text))
+                {
+                    TxtNome.Text = v[j].Nome;
+                    TxtPeso.Text = v[j].Peso.ToString();
+                    TxtAltura.Text = v[j].Altura.ToString();
+                    TxtIdade.Text = v[j].Idade.ToString();
+                    TxtCamisa.Text = v[j].Camisa.ToString();
+                    TxtTime.Text = v[j].Time;
+                    j = 2;
+                }
+                else
+                {
+                    if (j == 2) MessageBox.Show("O jogador informado não foi encontrado!");
+                }
+
+            }
+        }
+    }
+}
